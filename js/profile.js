2d874d09a79d1448760ae5741acd4c61380e59be
liyingpoh <liying.poh.2021@scis.smu.edu.sg>
2022-10-30 03:25:55 +0800

    editing user info kinda works

--- js/profile.js
+++ js/profile.js
@@ -1,3 +1,5 @@
+//////////////////////////////////////////////////
+// SELECTION OF CLUBS
 $(document).ready(function() {
   $(".country").submit(function() {
       let checked = $(":checkbox:checked").length
@@ -18,43 +20,41 @@ $(document).ready(function() {
 });
 
 
-
-
 //////////////////////////////////////////////////
 // EDIT BUTTON
-	var incompleteTasksHolder = document.getElementById("details"); //incomplete-tasks
-
-	//Edit an existing task
-	var editTask = function() {
-		var listItem = this.parentNode;
-		var editInput = listItem.querySelector("input[type=text]");
-		var label = listItem.querySelector("label");
-		var containsClass = listItem.classList.contains("editMode");
-		if (containsClass) { label.innerText = editInput.value; 
-		} else { editInput.value = label.innerText; }
-		listItem.classList.toggle("editMode");
-	}
-
-	//Mark a task as complete
-	var taskCompleted = function() {
-		var listItem = this.parentNode;
-		completedTasksHolder.appendChild(listItem);
-		bindTaskEvents(listItem, taskIncomplete);
-	}
-	var bindTaskEvents = function(taskListItem) {
-		var editButton = taskListItem.querySelector("button.edit");
-		editButton.onclick = editTask;
-	}
-
-	for (var i = 0; i < incompleteTasksHolder.children.length; i++) {
-		bindTaskEvents(incompleteTasksHolder.children[i], taskCompleted);
-	}
+var incompleteTasksHolder = document.getElementById("details"); 
+var editTask = function() {
+	var listItem = this.parentNode;
+	var editInput = listItem.querySelector("input[type=text]");
+	var label = listItem.querySelector("label");
+	var containsClass = listItem.classList.contains("editMode");
+	if (containsClass) { label.innerText = editInput.value; 
+	} else { editInput.value = label.innerText; }
+	listItem.classList.toggle("editMode");
+}
+var taskCompleted = function() {
+	var listItem = this.parentNode;
+	completedTasksHolder.appendChild(listItem);
+	bindTaskEvents(listItem, taskIncomplete);
+}
+var bindTaskEvents = function(taskListItem) {
+	var editButton = taskListItem.querySelector("button.edit");
+	editButton.onclick = editTask;
+}
+for (var i = 0; i < incompleteTasksHolder.children.length; i++) {
+	bindTaskEvents(incompleteTasksHolder.children[i], taskCompleted);
+}
 
 
 //////////////////////////////////////////////////
-// FIREBASE
+// FIREBASE IMPORT
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.13.0/firebase-app.js";
+import { getDatabase, ref, onValue , set } from
+"https://www.gstatic.com/firebasejs/9.13.0/firebase-database.js"
+import { getStorage, ref as sref, uploadBytesResumable, getDownloadURL } from "https://www.gstatic.com/firebasejs/9.13.0/firebase-storage.js";
 
+//////////////////////////////////////////////////
+// FIREBASE VARIABLES
 const firebaseConfig = {
   apiKey: "AIzaSyC_sRHAqy76KR30qWRWTT1HjahFEN0IN4Q",
   authDomain: "calendaready-g7t7.firebaseapp.com",
@@ -65,63 +65,43 @@ const firebaseConfig = {
   appId: "1:544037155570:web:c7e3ca7a1c55beaea8966b",
   measurementId: "G-03K9PHBX7D"
 };
-
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-
-
-/* CONNECT TO DATABASE */
-import { getDatabase, ref, onValue } from
-"https://www.gstatic.com/firebasejs/9.13.0/firebase-database.js"
-
 const db = getDatabase();
-
-// Get a reference to the data 'title'
 const users = ref(db, 'users') 
 
-// firebase console object syntax: { "creator": "Robin", "roomName": "Holy Robert Louis Stevenson" }
 
+//////////////////////////////////////////////////
+// FIREBASE POPULATE DETAILS
 onValue(users, (snapshot => {
-	const data = snapshot.val(); // get the new value
-	// console.log(data);
+	const data = snapshot.val(); 
 
 	let userInfo = data.user1.user_profile_info
-	// console.log(userInfo);
-	// console.log(userInfo.name);
-
 	for (let category in userInfo) {
 		if (category != "profile_picture") {
-			// console.log(category, userInfo[category]);
 			document.getElementById(category).innerText = userInfo[category];
 		}
 	}
-	// console.log(userInfo["profile_picture"]);
-	// console.log(document.getElementById('imagePreview').style.backgroundImage );
-	document.getElementById('imagePreview').style.backgroundImage = `url(${userInfo["profile_picture"]})`
+	document.getElementById('imagePreview').style.backgroundImage = userInfo["profile_picture"]
 }));
 
+
+//////////////////////////////////////////////////
+// FIREBASE POPULATE INTERESTED CLUBS
 onValue(users, (snapshot => {
 	const data = snapshot.val(); // get the new value
 
-	// console.log(data);
-
+	let tempHTML = ""
 	let userClubs = data.user1.user_clubs
-
-	// console.log(userClubs);
 
 	for (let union in userClubs) {
 		if (Object.hasOwnProperty.call(userClubs, union)) {
 			let unionClubs = userClubs[union];
 
-			// console.log(unionClubs);
-
 			for (let club in unionClubs) {
 				if (Object.hasOwnProperty.call(unionClubs, club)) {
 					let clubDetails = unionClubs[club];
 
-					// console.log(clubDetails);
-
-					document.getElementById('interested-club').innerHTML += `
+					tempHTML += `
 					<div class="card border-0" style="width: 15rem; height: 260px;">
 						<div class="image-center" >
 							<img style="border-radius: 50%; height: 8rem; width: auto;" src="${clubDetails["photo"]}" class="card-img-top" alt="...">
@@ -136,13 +116,12 @@ onValue(users, (snapshot => {
 						</div>
 					</div>
 					`;
-					
 				}
 			}
 		}
 	}
 	// ADD BUTTON
-	document.getElementById('interested-club').innerHTML += `
+	tempHTML += `
 	<div class="card border-0" style="width: 15rem; height: 260px">
 		<div class="image-center">
 			<img style="border-radius: 50%; width: 8rem; " src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/57/Circled_plus.svg/800px-Circled_plus.svg.png" class="card-img-top" alt="...">
@@ -159,34 +138,13 @@ onValue(users, (snapshot => {
 	</div>
 	`
 
+	document.getElementById('interested-club').innerHTML = tempHTML
+
 }));
 
-//////////////////////////////////////////////////
-// IMAGE
 
 //////////////////////////////////////////////////
 // CHANGE IMAGE
-// function readURL(input) {
-//     if (input.files && input.files[0]) {
-//         var reader = new FileReader();
-//         reader.onload = function(e) {
-//             $('#imagePreview').css('background-image', 'url('+e.target.result +')');
-//             $('#imagePreview').hide();
-//             $('#imagePreview').fadeIn(650);
-//         }
-//         reader.readAsDataURL(input.files[0]);
-//     }
-
-//     console.log("i'm in");
-
-// 	file = input.files[0]
-// }
-// $("#imageUpload").change(function() {
-//     readURL(this);
-// });
-
-import { getStorage, ref as sref, uploadBytesResumable, getDownloadURL } from "https://www.gstatic.com/firebasejs/9.13.0/firebase-storage.js";
-
 function readURL(input) {
     if (input.files && input.files[0]) {
         var reader = new FileReader();
@@ -198,97 +156,172 @@ function readURL(input) {
         reader.readAsDataURL(input.files[0]);
     }
 
-    console.log("i'm in");
-
+    // FIREBASE
 	let file = input.files[0]
 	
 	const storage = getStorage();
-	
-	// Create the file metadata
 	/** @type {any} */
 	const metadata = {
 	  contentType: 'image/jpeg'
 	};
 	
-	// Upload file and metadata to the object 'images/mountains.jpg'
 	const storageRef = sref(storage, 'images/' + file.name);
 	const uploadTask = uploadBytesResumable(storageRef, file, metadata);
 	
-	// Listen for state changes, errors, and completion of the upload.
 	uploadTask.on('state_changed',
-	  (snapshot) => {
-		// Get task progress, including the number of bytes uploaded and the total number of bytes to be uploaded
-		const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
-		console.log('Upload is ' + progress + '% done');
-		switch (snapshot.state) {
-		  case 'paused':
-			console.log('Upload is paused');
-			break;
-		  case 'running':
-			console.log('Upload is running');
-			break;
-		}
-	  }, 
-	  (error) => {
-		// A full list of error codes is available at
-		// https://firebase.google.com/docs/storage/web/handle-errors
-		switch (error.code) {
-		  case 'storage/unauthorized':
-			// User doesn't have permission to access the object
-			break;
-		  case 'storage/canceled':
-			// User canceled the upload
-			break;
-	
-		  // ...
-	
-		  case 'storage/unknown':
-			// Unknown error occurred, inspect error.serverResponse
-			break;
-		}
-	  }, 
-	  () => {
-		// Upload completed successfully, now we can get the download URL
+		(snapshot) => {
+			const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+			console.log('Upload is ' + progress + '% done');
+			switch (snapshot.state) {
+				case 'paused':
+					console.log('Upload is paused');
+					break;
+				case 'running':
+					console.log('Upload is running');
+					break;
+			}
+		}, 
+		(error) => {
+			switch (error.code) {
+			case 'storage/unauthorized':
+				break;
+			case 'storage/canceled':
+				break;
+			case 'storage/unknown':
+				break;
+			}
+		}, 
+	() => {
 		getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
-		  console.log('File available at', downloadURL);
-		});
-	  }
+			console.log('File available at', downloadURL);
+			});
+		}
 	);
 
 	// DOWNLOAD
-	// Create a reference to the file we want to download
 	const profileRef = sref(storage, `images/${file.name}`);
 	
 	// Get the download URL
 	getDownloadURL(profileRef)
-	  .then((url) => {
-		// UPDATE JSON WITH THIS URL
-		console.log(url);
-	  })
-	  .catch((error) => {
-		// A full list of error codes is available at
-		// https://firebase.google.com/docs/storage/web/handle-errors
-		switch (error.code) {
-		  case 'storage/object-not-found':
-			// File doesn't exist
-			break;
-		  case 'storage/unauthorized':
-			// User doesn't have permission to access the object
-			break;
-		  case 'storage/canceled':
-			// User canceled the upload
-			break;
-	
-		  // ...
-	
-		  case 'storage/unknown':
-			// Unknown error occurred, inspect the server response
-			break;
-		}
-	  });
+	    .then((url) => {
+			// UPDATE JSON WITH THIS URL
+			console.log(url);
+	  	})
+	  	.catch((error) => {
+			switch (error.code) {
+		  		case 'storage/object-not-found':
+					break;
+		  		case 'storage/unauthorized':
+					break;
+		  		case 'storage/canceled':
+					break;
+		  		case 'storage/unknown':
+					break;
+			}
+		});
 	
 }
 $("#imageUpload").change(function() {
     readURL(this);
 });
 
+
+// function res(item) {
+// 	console.log(item);
+// 	console.log("im in");
+// 	console.log(item.value);
+// }
+// document.getElementById("myBtn").addEventListener("click", res(this));
+
+
+// function writeUserData(userId, name, username, gender, birthday, email, matric_no, phone_no) {
+// 	const db = getDatabase();
+// 	set(ref(db, 'users/' + userId + '/user_profile_info'), {
+// 		name: name,
+// 		username: username,
+// 		gender: gender,
+// 		birthday: birthday,
+// 		email: email,
+// 		matric_no: matric_no,
+// 		phone_no: phone_no
+
+// 	});
+
+// 	console.log("Im in");
+// }
+
+// const apps = Vue.createApp({
+// 	el: "#info",
+
+//     // Data Properties
+//     data() {
+//         return {
+
+// 			name: "Natasha",
+// 			username: "",
+// 			gender: "",
+// 			birthday: "",
+// 			email: "",
+// 			matric_no: "",
+// 			phone_no: "",
+
+//         }
+//     },
+
+//     methods: {
+
+// 		writeUserData(userId, name, username, gender, birthday, email, matric_no, phone_no) {
+// 			const db = getDatabase();
+// 			set(ref(db, 'users/' + userId + '/user_profile_info'), {
+// 				name: name,
+// 				username: username,
+// 				gender: gender,
+// 				birthday: birthday,
+// 				email: email,
+// 				matric_no: matric_no,
+// 				phone_no: phone_no
+		
+// 			});
+		
+// 			console.log("Im in");
+// 		}
+
+//     }
+// })
+
+
+// // DO NOT MODIFY THIS
+// apps.mount("#info")
+
+// function writeUserData(userId, name, username, gender, birthday, email, matric_no, phone_no) {
+// 	const db = getDatabase();
+// 	set(ref(db, 'users/' + userId + '/user_profile_info'), {
+// 		name: name,
+// 		username: username,
+// 		gender: gender,
+// 		birthday: birthday,
+// 		email: email,
+// 		matric_no: matric_no,
+// 		phone_no: phone_no
+
+// 	})
+// }
+
+function writeUserData() {
+	const db = getDatabase();
+	set(ref(db, 'users/' + "user1" + '/user_profile_info'), {
+		name: document.getElementById('name').innerText,
+		username: document.getElementById('username').innerText,
+		gender: document.getElementById('gender').innerText,
+		birthday: document.getElementById('birthday').innerText,
+		email: document.getElementById('email').innerText,
+		matric_no: document.getElementById('matric_no').innerText,
+		phone_no: document.getElementById('phone_no').innerText, 
+		profile_picture: document.getElementById('imagePreview').style.backgroundImage
+
+	})
+
+	console.log("change success");
+}
+
+document.getElementById('save').addEventListener("click", writeUserData)
