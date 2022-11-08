589824d2da884c0e84dfa2e6e484220f767d94c5
Kanika Selvapandian <kanikas.2021@scis.smu.edu.sg>
2022-11-08 12:16:17 +0800

    Update explore.js

--- js/explore.js
+++ js/explore.js
@@ -31,15 +31,26 @@ const explorePage = Vue.createApp({
     data() { 
         return { 
             hello: "testing",
-            events: '',
+            display_events: '',
+            db_events: '', // this stores all events extracted from db
             userInfo: '',
+
+            //filter inputs
+            filter_club: [],
+            filter_event_type: [],
+            filter_start_date: null,
+            filter_end_date: null,
+            filter_min_price: null,
+            filter_max_price: null
+
+
             // added properties for GET_FILTER_CRITERIAS
-            org_club: '',
-            event_type: '',
-            start_date: '',
-            end_date: '',
-            min_price: '',
-            max_price: ''
+            // org_club: '',
+            // event_type: '',
+            // start_date: '',
+            // end_date: '',
+            // min_price: '',
+            // max_price: ''
         };
     }, // data
     computed: { 
@@ -53,52 +64,95 @@ const explorePage = Vue.createApp({
         console.log("====Function-GETALLEVENTS===")
         onValue(allEvents, (snapshot) => {
             const data = snapshot.val()
-            console.log("-------In mounted------");
+            console.log("-------In event mounted------");
             console.log(data);
-            this.events = data
-            console.log(this.events);
+            this.db_events = data
+            this.display_events = data
+            console.log(this.db_events);
+            console.log("-------end event mounted------");
         })
         onValue(users, (snapshot) => {
             const data = snapshot.val()
-            console.log("-------In mounted------");
+            console.log("-------In user mounted------");
             console.log(data);
             this.userInfo = data.user1.user_profile_info
             console.log(this.userInfo);
+            console.log("-------end user  mounted------");
         })
     },
     methods: {
         // JL: To get all the inputs from the filter
-        get_filter_criterias() {
-            console.log("====Function-get_filter_criterias()S===")
-            this.org_club = document.getElementById('org_club').value;
-            console.log(this.org_club)
-            this.event_type = document.getElementById('event_type').value;
-            console.log(this.event_type)
-            this.start_date = document.getElementById('start_date').value;
-            console.log(this.start_date)
-            this.end_date = document.getElementById('end_date').value;
-            console.log(this.end_date)
-            this.min_price = document.getElementById('min_price').value;
-            console.log(this.min_price)
-            this.max_price = document.getElementById('max_price').value;
-            console.log(this.max_price)
-            console.log("====FunctionEND-get_filter_criterias()===")
+        // get_filter_criterias() {
+        //     console.log("====Function-get_filter_criterias()S===")
+        //     this.org_club = document.getElementById('org_club').value;
+        //     console.log(this.org_club)
+        //     this.event_type = document.getElementById('event_type').value;
+        //     console.log(this.event_type)
+        //     this.start_date = document.getElementById('start_date').value;
+        //     console.log(this.start_date)
+        //     this.end_date = document.getElementById('end_date').value;
+        //     console.log(this.end_date)
+        //     this.min_price = document.getElementById('min_price').value;
+        //     console.log(this.min_price)
+        //     this.max_price = document.getElementById('max_price').value;
+        //     console.log(this.max_price)
+        //     console.log("====FunctionEND-get_filter_criterias()===")
 
-            // onFilter_org_club(this.org_club)
-            console.log("====Function-onFilter_org_club===")
-            let filtered_events = {}
-            console.log("ffff")
-            console.log(this.events)
-            let allevents = this.events;
-            for (let event1 in allevents) {
-                console.log(event1)
-                let event_club = JSON.stringify(event1.club)
-                console.log(event_club)
-            }
+        //     // onFilter_org_club(this.org_club)
+        //     console.log("====Function-onFilter_org_club===")
+        //     let filtered_events = {}
+        //     console.log("ffff")
+        //     console.log(this.events)
+        //     let allevents = this.events;
+        //     for (let event1 in allevents) {
+        //         console.log(event1)
+        //         let event_club = JSON.stringify(event1.club)
+        //         console.log(event_club)
+        //     }
             
-            console.log("====FunctionEND-onFilter_org_club===")
 
+        //     console.log("====FunctionEND-onFilter_org_club===")
+
+        // },
+
+        // filter works, to be completed
+        filter_events() {
+            console.log("====Function-filter_events()===")
+
+            console.log(this.db_events);
+            let all_events= this.db_events
+
+            let filtered_obj = {}
+
+            //loop thru all event from db_events
+            for (let [event, details] of Object.entries(all_events)) {
+                // console.log(details);
+                // console.log(event);
+
+                // check if user selected any clubs to filter and if they did, extract those events
+                if (this.filter_club.length != 0 && this.filter_club.includes( details.club )) {
+                    console.log(details);
+                    // filtered_arr.push({event: details})
+                    filtered_obj[event] = details
+                    console.log(filtered_obj);
+                }
+                if (this.filter_event_type.length != 0 && this.filter_event_type.includes( details.type )) {
+                    console.log(details);
+                    // filtered_arr.push({event: details})
+                    filtered_obj[event] = details
+                    console.log(filtered_obj);
+                }
+            }
+
+            this.display_events = filtered_obj
+            console.log("====FunctionEND-filter_events()===")
         },
+
+
+
+
+
+
         // JL: coz my filter doesnt work so
         // i decieded to get filter by org-club first at least
         onFilter_org_club(org_club_input) {
@@ -121,30 +175,30 @@ const explorePage = Vue.createApp({
             })
         },
 
-        onFilter() {
+        // onFilter() {
             
-            var club = document.getElementById("org_club").value;
-            var type = document.getElementById("event_type").value;
-            var s_date = document.getElementById("start_date").value;
-            var e_date = document.getElementById("end_date").value;
-            var min_price = document.getElementById("min_price").value;
-            var max_price = document.getElementById("max_price").value;
+        //     var club = document.getElementById("org_club").value;
+        //     var type = document.getElementById("event_type").value;
+        //     var s_date = document.getElementById("start_date").value;
+        //     var e_date = document.getElementById("end_date").value;
+        //     var min_price = document.getElementById("min_price").value;
+        //     var max_price = document.getElementById("max_price").value;
 
-            const que = query(allEvents,orderByChild("club"), equalTo("club"));
+        //     const que = query(allEvents,orderByChild("club"), equalTo("club"));
 
-            filtered_events = []
+        //     filtered_events = []
 
-            get(que)
-            .then((snapshot)=> {
-                snapshot.forEach(childsnapshot => {
-                    filtered_events.push(childsnapshot.val());
+        //     get(que)
+        //     .then((snapshot)=> {
+        //         snapshot.forEach(childsnapshot => {
+        //             filtered_events.push(childsnapshot.val());
                     
-                });
+        //         });
             
-            console.log(filtered_events)    
-            return filtered_events;
-            })
-        }
+        //     console.log(filtered_events)    
+        //     return filtered_events;
+        //     })
+        // }
     } // methods
 });
 
